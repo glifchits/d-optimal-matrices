d8d23b8e88e786e1ef3812b1434879803768e95a
George Lifchits <george.lifchits@gmail.com>
2015-09-16 21:27:55 -0400

    refactored `paf` `psd` `dft` methods into `lib.py`

--- doptimal-9.py
+++ doptimal-9.py
@@ -1,23 +1,4 @@
-import cmath
-
-
-def paf(seq, i):
-    n = len(seq)
-    pairs = [seq[k] * seq[(k+i) % n] for k in range(n)]
-    return sum(pairs)
-
-
-def dft(seq, s):
-    n = len(seq)
-    omega = cmath.e ** ((2 * cmath.pi * 1j) / n)
-    terms = [seq[k] * omega ** (k+s) for k in range(n)]
-    return sum(terms)
-
-
-def psd(seq, s):
-    n = len(seq)
-    d = dft(seq, s)
-    return d.real ** 2 + d.imag ** 2
+from lib import *
 
 
 if __name__ == '__main__':
