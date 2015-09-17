f78e750cba5c59bd77d54a53591a4c38db869aca
George Lifchits <george.lifchits@gmail.com>
2015-09-16 21:26:25 -0400

    implementation of solution to Kotsireas example email

---
+++ doptimal-9.py
@@ -0,0 +1,51 @@
+import cmath
+
+
+def paf(seq, i):
+    n = len(seq)
+    pairs = [seq[k] * seq[(k+i) % n] for k in range(n)]
+    return sum(pairs)
+
+
+def dft(seq, s):
+    n = len(seq)
+    omega = cmath.e ** ((2 * cmath.pi * 1j) / n)
+    terms = [seq[k] * omega ** (k+s) for k in range(n)]
+    return sum(terms)
+
+
+def psd(seq, s):
+    n = len(seq)
+    d = dft(seq, s)
+    return d.real ** 2 + d.imag ** 2
+
+
+if __name__ == '__main__':
+    aa = [1, 1, 1, 1, 1, 1, 1, -1, -1]
+    bb = [1, 1, -1, 1, -1, 1, 1, 1, -1]
+    a = sum(aa)
+    b = sum(bb)
+
+    print "a = {0}, b = {1}".format(sum(aa), sum(bb))
+    print ""
+    print "Checking diophantine equation (expect 34)"
+    print "a^2 + b^2 = {0}".format(sum(aa) ** 2 + sum(bb) ** 2)
+    print ""
+    print "Checking PSDs (expect sum to be 16)"
+
+    for s in range(len(aa)):
+        psd_a = psd(aa, s)
+        psd_b = psd(bb, s)
+        print "S {3:2}    PSD(A) {0:.5f}  PSD(B) {1:.5f}  PSD(A)+PSD(B) {2:.5f}".format(
+            psd_a, psd_b, psd_a+psd_b, s
+        )
+
+    print ""
+    print "Checking PAF (expect sum to be 2)"
+
+    for s in range(len(aa)):
+        paf_a = paf(aa, s)
+        paf_b = paf(bb, s)
+        print "S {3:2}    PAF(A) {0:.5f}  PAF(B) {1:8.5f}  PAF(A)+PAF(B) {2:.5f}".format(
+            paf_a, paf_b, paf_a+paf_b, s
+        )
